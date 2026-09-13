66fcf2632fc4be3c1207c0232fe8f42ffd7f4b23
bankofzoabi <zoabimohamed191@gmail.com>
2026-09-13 18:51:54 +0300

    create math test

---
+++ mathtest.py
@@ -0,0 +1,74 @@
+a = 5
+b = 7
+c = a * b 
+#print (c)
+d = a + b
+#print (d)
+e = a - b
+#print (e)
+f = a / b
+#print (f)
+a = 50
+g = a // b
+#print (g)
+#print ( g + 3 )
+a = 137
+b = 20 
+c = a % b
+#print (c) 
+a = 5
+b = 3
+c = a ** b
+#print (c)
+#print ( a == b)
+a = 10 
+b = 10 
+#print ( a == b)
+a = 20 
+b = 5
+c = (a > b )
+#print (c)
+c=(a < b )
+#print (c)
+c=(a >= b )
+#print (c)
+c=(a <= b )
+#print (c)
+a = 20 
+b = 20 
+c = (a <= b )
+#print (c)
+c = (a >= b )
+#print (c)
+a = -20
+b = 50
+c = ( 10 < a < 30 )
+#print (c)
+c = abs(a)
+#print (c)
+a = 3.99887
+c = int(a)
+#print (c)
+a = 5 
+b = 9 
+c= complex(a , b)
+#print (c)
+a = 50 
+b = 7 
+c = divmod(a , b)
+#print (c)
+a = 5
+b = 7   
+c = pow(a , b)
+#print (c)
+c = divmod ( a , b + 1)
+#print (c)
+a = 5
+b = 13
+c = 29 
+d = 4
+e = 8 
+f = ( a + b ) * ( c - d ) / e
+#print (f)
+x = (c - b) / (e + d + a)
+print (x)
