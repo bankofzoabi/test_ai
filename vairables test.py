1c8e9862032a292d4aeceb70ac0fb47f9671e4f7
bankofzoabi <zoabimohamed191@gmail.com>
2026-09-13 15:48:42 +0300

    upload vairables test

---
+++ vairables test.py
@@ -0,0 +1,59 @@
+a = 5 
+b = 'ahmad' 
+c = 12 
+print (type(b))
+print (type(a))
+print (type(c))
+thiscase = True
+thatcase = False
+print (type(thiscase))
+print (thatcase)
+print (thiscase)
+print (thiscase and thatcase)
+print (not thatcase)
+print (not thiscase)
+f = 4.5
+print (f)
+print (a , b , c , f , thiscase , thatcase)
+g = 5j + 7 
+print (type(g))
+print (g)
+w = str (f)
+print (type(w))
+print ( w ) 
+h = "235" 
+r = float(h)
+print (type(r))
+print (r)
+a = 5
+b = int(a)
+c = float(a)
+d = 7 
+e = str(d)
+f = bool(b)
+f = complex(b , d )
+print ( a , b , c , d , e , f )
+t = bin(5)
+print (type(t))
+print (t)
+z = bool(4) 
+print (type(z))
+print (z)
+r = bool(0)
+print (type(r))
+print (r)
+m = bool ( ' yosef')
+print (type(m))
+print (m)
+a = 5
+b = 7
+c = 5.2 
+d = 8j 
+e = ( a + b ) *( c + d )
+print (type(e))
+print (e)
+a = " balnce acount " 
+print(f"{a} 1000")
+name = " ' "
+print(name * 10)
+print("-" * 100)
