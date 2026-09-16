1ccd7a7c6f11d6725e38ddac4a13fd5f8c7e8cb4
bankofzoabi <zoabimohamed191@gmail.com>
2026-09-16 19:10:26 +0300

    createstringtest.py

---
+++ stringtest.py
@@ -0,0 +1,17 @@
+a = " (how are you) "
+b = a[4]
+#print (b)
+c = [-2]
+#print (a[-2])
+b = (a*30)
+#print (b)
+#print (b*40)
+#print (a[2:8])
+#print (a[:12])
+#print (a[4:])
+#print (a[:15])
+#print (a[:-4])
+#print (a[0:9]) 
+b = (a[0:7])
+c = (a[7:-1])
+print (b.c)
