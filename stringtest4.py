3d250b3c834ca5881fc7ceca02f36bdc1f476c49
bankofzoabi <zoabimohamed191@gmail.com>
2026-09-18 17:56:39 +0300

    createstringtest4.py

---
+++ stringtest4.py
@@ -0,0 +1,17 @@
+a = "yosef is the best mentor"
+b = '1536756879632456'
+c = 'hello'
+d = "######## i love python***"
+e = "      smooking kills            "
+#print(a.ljust(30))
+#print(a.rjust(70))
+#print(a.rjust(30,'*'))
+#print(a.ljust(30,'*'))
+#print(b.zfill(70))
+#print(c.isalpha())
+#rint(e.strip())
+#print (e.rstrip())
+#print (e.lstrip())
+#print (d.strip('*,#'))
+#print (d.lstrip('#'))
+#print (d.strip('*'))
