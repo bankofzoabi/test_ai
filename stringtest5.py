45092c99a3ea5a63951299d4632991e667939ba2
bankofzoabi <zoabimohamed191@gmail.com>
2026-09-19 16:36:14 +0300

    createstring5.py

---
+++ stringtest5.py
@@ -0,0 +1,29 @@
+a = "yosef is \the best mentor"
+b = "yosef is \tthe best mentor"#print (a)
+print (b)
+c = "yosef is \nthe best mentor"
+print (c)
+d = r'C:\ysoef \my \mentor'
+print (d,b)
+e = "yosef is \t\t\t\t\t\t\tmy mentor"
+print (e)
+f = """yosef my mentor
+dckxbjbiodfjsviojfcsizosjb
+fbdfgbf dxfghgvdfdgb. rgfv
+bdhghbfbgf fghsd"""
+h = f.splitlines()
+print (f)
+x = 'python'
+y = 'yosef'
+print (x, end = '')
+print (y,a,b,c,d,e,f,h)
+z = 'yosef'
+t = 'my mentor is %s' %y
+print (t)
+u = "AI ENGINEER"
+w = 47
+p = "AI ENGINEER"
+k ="i\'m studying %s and it\'s take %d weeks to be %s " %(u,w,p)
+print (k)
+l = "i\'m studying %50s and it\'s take %20d weeks to be %16s " %(u,w,p)
+print (l)
